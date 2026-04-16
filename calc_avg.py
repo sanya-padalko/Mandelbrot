5966eef063240a9498d77a71459fb921507390f4
sanya-padalko <apadalko90@gmail.com>
2026-04-16 16:10:06 +0300

    Убран копипаст
    Из python-скрипта убран копипаст

--- calc_avg.py
+++ calc_avg.py
@@ -8,165 +8,33 @@ def analyze(filename):
     std = np.std(data)
     return avg, std
 
-file1 = 'results/results_base'
-file2 = 'results/results_opt'
-file3 = 'results/results_avx'
+def print_result(filename, label):
+    res = []
+    s_avg = []
+    for i in range(5):
+        file_name = filename + str(i + 1) + '.txt'
+        res.append(analyze(file_name))
+        s_avg.append(res[i][0])
 
-res_base = []
-res_opt  = []
-res_avx  = []
-res_base_O3 = []
-res_opt_O3  = []
-res_avx_O3  = []
+    s_avg.sort()
+    avg = sum(s_avg[1:4]) / 3
+    std_s = 0
+    for i in range(5):
+        std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
+    std_s = sqrt(std_s / 5)
+    for i in range(5):
+        print(f"Base #{i + 1}: {int(s_avg[i])} ticks")
 
-s_avg = []
+    print()
+    print(f"Relative error: {(std_s / avg * 100):.2}%")
+    print(f"{label}: {int(avg)} ticks")
+    s_avg = []
+    res_base = []
+    print()
 
-for i in range(5):
-    file_name = file1 + str(i + 1) + '.txt'
-    res_base.append(analyze(file_name))
-    avg = res_base[i][0]
-    s_avg.append(avg)
-
-s_avg.sort()
-avg = sum(s_avg[1:4]) / 3
-std_s = 0
-for i in range(5):
-    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
-std_s = sqrt(std_s / 5)
-for i in range(5):
-    print(f"Base #{i + 1}: {int(s_avg[i])} ticks")
-
-print()
-print(f"Relative error: {(std_s / avg * 100):.2}%")
-print(f"Base avg: {int(avg)} ticks")
-s_avg = []
-res_base = []
-print()
-
-
-for i in range(5):
-    file_name = file1 + '_O3_' + str(i + 1) + '.txt'
-    res_base.append(analyze(file_name))
-    avg = res_base[i][0]
-    s_avg.append(avg)
-
-s_avg.sort()
-avg = sum(s_avg[1:4]) / 3
-std_s = 0
-for i in range(5):
-    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
-
-std_s = sqrt(std_s / 5)
-
-for i in range(5):
-    print(f"Base O3 #{i + 1}: {int(s_avg[i])} ticks")
-
-print()
-print(f"Relative error: {(std_s / avg * 100):.2}%")
-print(f"Base O3 avg: {int(avg)} ticks")
-s_avg = []
-res_base = []
-print()
-
-for i in range(5):
-    file_name = file2 + str(i + 1) + '.txt'
-    res_base.append(analyze(file_name))
-    avg = res_base[i][0]
-    s_avg.append(avg)
-
-s_avg.sort()
-avg = sum(s_avg[1:4]) / 3
-std_s = 0
-for i in range(5):
-    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
-
-std_s = sqrt(std_s / 5)
-
-for i in range(5):
-    print(f"Opt (8x8) #{i + 1}: {int(s_avg[i])} ticks")
-
-print()
-print(f"Relative error: {(std_s / avg * 100):.2}%")
-print(f"Opt (8x8) avg: {int(avg)} ticks")
-s_avg = []
-res_base = []
-print()
-
-
-
-for i in range(5):
-    file_name = file2 + '_O3_' + str(i + 1) + '.txt'
-    res_base.append(analyze(file_name))
-    avg = res_base[i][0]
-    s_avg.append(avg)
-
-s_avg.sort()
-avg = sum(s_avg[1:4]) / 3
-std_s = 0
-for i in range(5):
-    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
-
-std_s = sqrt(std_s / 5)
-
-for i in range(5):
-    print(f"Opt (8x8) O3 #{i + 1}: {int(s_avg[i])} ticks")
-
-print()
-print(f"Relative error: {(std_s / avg * 100):.2}%")
-print(f"Opt (8x8) O3 avg: {int(avg)} ticks")
-s_avg = []
-res_base = []
-print()
-
-
-
-for i in range(5):
-    file_name = file3 + str(i + 1) + '.txt'
-    res_base.append(analyze(file_name))
-    avg = res_base[i][0]
-    s_avg.append(avg)
-
-s_avg.sort()
-avg = sum(s_avg[1:4]) / 3
-std_s = 0
-for i in range(5):
-    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
-
-std_s = sqrt(std_s / 5)
-
-for i in range(5):
-    print(f"AVX #{i + 1}: {int(s_avg[i])} ticks")
-
-print()
-print(f"Relative error: {(std_s / avg * 100):.2}%")
-print(f"AVX avg: {int(avg)} ticks")
-s_avg = []
-res_base = []
-print()
-
-
-
-for i in range(5):
-    file_name = file3 + '_O3_' + str(i + 1) + '.txt'
-    res_base.append(analyze(file_name))
-    avg = res_base[i][0]
-    s_avg.append(avg)
-
-s_avg.sort()
-avg = sum(s_avg[1:4]) / 3
-std_s = 0
-for i in range(5):
-    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
-
-std_s = sqrt(std_s / 5)
-
-for i in range(5):
-    print(f"AVX O3 #{i + 1}: {int(s_avg[i])} ticks")
-
-print()
-print(f"Relative error: {(std_s / avg * 100):.2}%")
-print(f"AVX O3 avg: {int(avg)} ticks")
-s_avg = []
-res_base = []
-print()
-
+print_result('results/results_base', 'Base avg')
+print_result('results/results_base_O3_', 'Base O3 avg')
+print_result('results/results_opt', 'Opt avg')
+print_result('results/results_opt_O3_', 'Opt O3 avg')
+print_result('results/results_avx', 'AVX avg')
+print_result('results/results_avx_O3_', 'AVX O3 avg')
