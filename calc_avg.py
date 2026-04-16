5b802e453bd68d2a58fbe5b14e54e9d6eb07e50b
sanya-padalko <apadalko90@gmail.com>
2026-04-16 15:48:05 +0300

    Добавлена относительная погрешность

--- calc_avg.py
+++ calc_avg.py
@@ -25,46 +25,69 @@ for i in range(5):
     file_name = file1 + str(i + 1) + '.txt'
     res_base.append(analyze(file_name))
     avg = res_base[i][0]
-    std = res_base[i][1]
-    std = 2.78 * sqrt(std / 4) / sqrt(5)
-
-    print(f"Base #{i + 1}: {int(avg)} +- {int(std)} ticks")
     s_avg.append(avg)
 
+s_avg.sort()
+avg = sum(s_avg[1:4]) / 3
+std_s = 0
+for i in range(5):
+    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
+std_s = sqrt(std_s / 5)
+for i in range(5):
+    print(f"Base #{i + 1}: {int(s_avg[i])} ticks")
+
 print()
-print(f"Base avg: {int((sum(s_avg) - min(s_avg) - max(s_avg)) / 3)}")
+print(f"Relative error: {(std_s / avg * 100):.2}%")
+print(f"Base avg: {int(avg)} ticks")
 s_avg = []
+res_base = []
 print()
 
 
 for i in range(5):
     file_name = file1 + '_O3_' + str(i + 1) + '.txt'
-    res_base_O3.append(analyze(file_name))
-    avg = res_base_O3[i][0]
-    std = res_base_O3[i][1]
-    std = 2.78 * sqrt(std / 4) / sqrt(5)
-
-    print(f"Base O3 #{i + 1}: {int(avg)} +- {int(std)} ticks")
+    res_base.append(analyze(file_name))
+    avg = res_base[i][0]
     s_avg.append(avg)
 
-print()
-print(f"Base O3 avg: {int((sum(s_avg) - min(s_avg) - max(s_avg)) / 3)}")
-s_avg = []
-print()
+s_avg.sort()
+avg = sum(s_avg[1:4]) / 3
+std_s = 0
+for i in range(5):
+    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
 
+std_s = sqrt(std_s / 5)
+
+for i in range(5):
+    print(f"Base O3 #{i + 1}: {int(s_avg[i])} ticks")
+
+print()
+print(f"Relative error: {(std_s / avg * 100):.2}%")
+print(f"Base O3 avg: {int(avg)} ticks")
+s_avg = []
+res_base = []
+print()
 
 for i in range(5):
     file_name = file2 + str(i + 1) + '.txt'
-    res_opt.append(analyze(file_name))
-    avg = res_opt[i][0]
-    std = res_opt[i][1]
-    std = 2.78 * sqrt(std / 4) / sqrt(5)
-
-    print(f"Opt #{i + 1}: {int(avg)} +- {int(std)} ticks")
+    res_base.append(analyze(file_name))
+    avg = res_base[i][0]
     s_avg.append(avg)
 
+s_avg.sort()
+avg = sum(s_avg[1:4]) / 3
+std_s = 0
+for i in range(5):
+    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
+
+std_s = sqrt(std_s / 5)
+
+for i in range(5):
+    print(f"Opt (8x8) #{i + 1}: {int(s_avg[i])} ticks")
+
 print()
-print(f"Opt avg: {int((sum(s_avg) - min(s_avg) - max(s_avg)) / 3)}")
+print(f"Relative error: {(std_s / avg * 100):.2}%")
+print(f"Opt (8x8) avg: {int(avg)} ticks")
 s_avg = []
 print()
 
@@ -72,50 +95,77 @@ print()
 
 for i in range(5):
     file_name = file2 + '_O3_' + str(i + 1) + '.txt'
-    res_opt_O3.append(analyze(file_name))
-    avg = res_opt_O3[i][0]
-    std = res_opt_O3[i][1]
-    std = 2.78 * sqrt(std / 4) / sqrt(5)
-
-    print(f"Opt O3 #{i + 1}: {int(avg)} +- {int(std)} ticks")
+    res_base.append(analyze(file_name))
+    avg = res_base[i][0]
     s_avg.append(avg)
 
+s_avg.sort()
+avg = sum(s_avg[1:4]) / 3
+std_s = 0
+for i in range(5):
+    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
+
+std_s = sqrt(std_s / 5)
+
+for i in range(5):
+    print(f"Opt (8x8) O3 #{i + 1}: {int(s_avg[i])} ticks")
+
 print()
-print(f"Opt O3 avg: {int((sum(s_avg) - min(s_avg) - max(s_avg)) / 3)}")
+print(f"Relative error: {(std_s / avg * 100):.2}%")
+print(f"Opt (8x8) O3 avg: {int(avg)} ticks")
 s_avg = []
+res_base = []
 print()
 
 
 
 for i in range(5):
     file_name = file3 + str(i + 1) + '.txt'
-    res_avx.append(analyze(file_name))
-    avg = res_avx[i][0]
-    std = res_avx[i][1]
-    std = 2.78 * sqrt(std / 4) / sqrt(5)
-
-    print(f"AVX #{i + 1}: {int(avg)} +- {int(std)} ticks")
+    res_base.append(analyze(file_name))
+    avg = res_base[i][0]
     s_avg.append(avg)
 
+s_avg.sort()
+avg = sum(s_avg[1:4]) / 3
+std_s = 0
+for i in range(5):
+    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
+
+std_s = sqrt(std_s / 5)
+
+for i in range(5):
+    print(f"AVX #{i + 1}: {int(s_avg[i])} ticks")
+
 print()
-print(f"AVX avg: {int((sum(s_avg) - min(s_avg) - max(s_avg)) / 3)}")
+print(f"Relative error: {(std_s / avg * 100):.2}%")
+print(f"AVX avg: {int(avg)} ticks")
 s_avg = []
+res_base = []
 print()
 
 
 
 for i in range(5):
     file_name = file3 + '_O3_' + str(i + 1) + '.txt'
-    res_avx_O3.append(analyze(file_name))
-    avg = res_avx_O3[i][0]
-    std = res_avx_O3[i][1]
-    std = 2.78 * sqrt(std / 4) / sqrt(5)
-
-    print(f"AVX O3 #{i + 1}: {int(avg)} +- {int(std)} ticks")
+    res_base.append(analyze(file_name))
+    avg = res_base[i][0]
     s_avg.append(avg)
 
+s_avg.sort()
+avg = sum(s_avg[1:4]) / 3
+std_s = 0
+for i in range(5):
+    std_s += (s_avg[i] - avg) * (s_avg[i] - avg)
+
+std_s = sqrt(std_s / 5)
+
+for i in range(5):
+    print(f"AVX O3 #{i + 1}: {int(s_avg[i])} ticks")
+
 print()
-print(f"AVX O3 avg: {int((sum(s_avg) - min(s_avg) - max(s_avg)) / 3)}")
+print(f"Relative error: {(std_s / avg * 100):.2}%")
+print(f"AVX O3 avg: {int(avg)} ticks")
 s_avg = []
+res_base = []
 print()
 
--- main_res.md
+++ main_res.md
@@ -1,47 +1,53 @@
-Base #1: 358699664 +- 2094 ticks
-Base #2: 358266208 +- 2107 ticks
-Base #3: 356191734 +- 2033 ticks
-Base #4: 358200876 +- 2020 ticks
-Base #5: 358717417 +- 2121 ticks
+Base #1: 356191734 ticks
+Base #2: 358200876 ticks
+Base #3: 358266208 ticks
+Base #4: 358699664 ticks
+Base #5: 358717417 ticks
 
-Base avg: 358388916
+Relative error: 0.28%
+Base avg: 358388916 ticks
 
-Base O3 #1: 130266618 +- 1487 ticks
-Base O3 #2: 130706481 +- 1625 ticks
-Base O3 #3: 129554051 +- 1426 ticks
-Base O3 #4: 130988525 +- 1578 ticks
-Base O3 #5: 131849457 +- 1647 ticks
+Base O3 #1: 129554051 ticks
+Base O3 #2: 130266618 ticks
+Base O3 #3: 130706481 ticks
+Base O3 #4: 130988525 ticks
+Base O3 #5: 131849457 ticks
 
-Base O3 avg: 130653875
+Relative error: 0.58%
+Base O3 avg: 130653875 ticks
 
-Opt #1: 717168953 +- 2605 ticks
-Opt #2: 717284032 +- 2574 ticks
-Opt #3: 716285715 +- 2474 ticks
-Opt #4: 716881290 +- 2364 ticks
-Opt #5: 718391602 +- 2458 ticks
+Opt (8x8) #1: 716285715 ticks
+Opt (8x8) #2: 716881290 ticks
+Opt (8x8) #3: 717168953 ticks
+Opt (8x8) #4: 717284032 ticks
+Opt (8x8) #5: 718391602 ticks
 
-Opt avg: 717111425
+Relative error: 0.097%
+Opt (8x8) avg: 717111425 ticks
 
-Opt O3 #1: 84946718 +- 1389 ticks
-Opt O3 #2: 84799624 +- 1330 ticks
-Opt O3 #3: 85627206 +- 1582 ticks
-Opt O3 #4: 85326553 +- 1437 ticks
-Opt O3 #5: 85120550 +- 1434 ticks
+Opt (8x8) O3 #1: 716285715 ticks
+Opt (8x8) O3 #2: 716881290 ticks
+Opt (8x8) O3 #3: 717168953 ticks
+Opt (8x8) O3 #4: 717284032 ticks
+Opt (8x8) O3 #5: 718391602 ticks
 
-Opt O3 avg: 85131274
+Relative error: 0.097%
+Opt (8x8) O3 avg: 717111425 ticks
 
-AVX #1: 160427522 +- 1666 ticks
-AVX #2: 160259231 +- 1735 ticks
-AVX #3: 160210476 +- 1665 ticks
-AVX #4: 160412201 +- 1694 ticks
-AVX #5: 161448501 +- 1763 ticks
+AVX #1: 160210476 ticks
+AVX #2: 160259231 ticks
+AVX #3: 160412201 ticks
+AVX #4: 160427522 ticks
+AVX #5: 161448501 ticks
 
-AVX avg: 160366318
+Relative error: 0.31%
+AVX avg: 160366318 ticks
 
-AVX O3 #1: 18451491 +- 918 ticks
-AVX O3 #2: 18497515 +- 1004 ticks
-AVX O3 #3: 18696639 +- 1037 ticks
-AVX O3 #4: 18516714 +- 1023 ticks
-AVX O3 #5: 18699918 +- 1087 ticks
+AVX O3 #1: 18451491 ticks
+AVX O3 #2: 18497515 ticks
+AVX O3 #3: 18516714 ticks
+AVX O3 #4: 18696639 ticks
+AVX O3 #5: 18699918 ticks
 
-AVX O3 avg: 18570289
+Relative error: 0.57%
+AVX O3 avg: 18570289 ticks
